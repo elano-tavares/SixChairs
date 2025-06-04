0a0bda1a50f9c91abad5b5504c39fc01ce1e2f7b
elano-tavares <elano.tavares.ncs@gmail.com>
2025-06-04 19:47:49 -0300

    criação do arquivo com a classe filme

---
+++ src/filme.py
@@ -0,0 +1,15 @@
+# filme.py
+
+#----------------#
+#  Classe_Filme  #
+#----------------#
+class Filme:
+    def __init__(self, id: str, titulo: str, ano: int, genero: str, diretor: str):
+        self.id = id
+        self.titulo = titulo
+        self.ano = ano
+        self.genero = genero
+        self.diretor = diretor
+    
+    def __repr__(self):
+        return f"[{self.id}] {self.titulo} ({self.ano}) - {self.genero} | Dir: {self.diretor}"
